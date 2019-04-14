95626667cce0eb704b469b0fde3b57dcdb19c8b5
roscas <sol.rosca@he-arc.ch>
2019-04-14 17:33:01 +0200

    assets

--- core/src/com/mygame/JigsawPuzzle.java
+++ core/src/com/mygame/JigsawPuzzle.java
@@ -1,12 +1,14 @@
 package com.mygame;
 
 import com.framework.BaseGame;
+import com.mygame.screens.LevelScreen;
 import com.mygame.screens.MainMenuScreen;
 
 public class JigsawPuzzle extends BaseGame {
 
 	public void create() {
 		super.create();
-	    setActiveScreen(new MainMenuScreen());
+	    // setActiveScreen(new MainMenuScreen());
+	    setActiveScreen(new LevelScreen());
     }
 }
---
+++ core/src/com/mygame/screens/LevelScreen.java
@@ -0,0 +1,23 @@
+package com.mygame.screens;
+
+import com.framework.BaseScreen;
+
+public class LevelScreen extends BaseScreen {
+
+	/*------------------------------------------------------------------*\
+	|*							Initialize  							*|
+	\*------------------------------------------------------------------*/
+
+    public void initialize() {
+
+    }
+
+    /*------------------------------------------------------------------*\
+   	|*							Public Methods 							*|
+   	\*------------------------------------------------------------------*/
+
+    @Override
+    public void update(float dt) {
+        
+    }
+}
